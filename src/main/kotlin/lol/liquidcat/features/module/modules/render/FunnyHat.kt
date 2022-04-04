47491f73c1ff625360b8e0f0cce8e193ea535cda
Shurpe <81492944+14ms@users.noreply.github.com>
2022-04-04 18:43:07 +0300

    Improved FunnyHat module

--- src/main/kotlin/lol/liquidcat/features/module/modules/render/FunnyHat.kt
+++ src/main/kotlin/lol/liquidcat/features/module/modules/render/FunnyHat.kt
@@ -10,9 +10,11 @@ import lol.liquidcat.event.Render3DEvent
 import lol.liquidcat.features.module.Module
 import lol.liquidcat.features.module.ModuleCategory
 import lol.liquidcat.utils.render.GLUtils
+import lol.liquidcat.value.BoolValue
 import lol.liquidcat.value.FloatValue
 import lol.liquidcat.value.IntValue
 import org.lwjgl.opengl.GL11.*
+import java.awt.Color
 import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
@@ -23,6 +25,9 @@ object FunnyHat : Module("FunnyHat", "funny china hat", ModuleCategory.RENDER) {
 
     private val radius by FloatValue("Radius", 0.6f, 0.5f..2f)
     private val height by FloatValue("Height", 0.3f, 0.1f..1f)
+
+    private val rainbow by BoolValue("Rainbow", true)
+    private val rainbowSpeed by IntValue("RainbowSpeed", 25, 1..50)
 
     private val red by IntValue("Red", 255, 0..255)
     private val green by IntValue("Green", 150, 0..255)
@@ -35,27 +40,33 @@ object FunnyHat : Module("FunnyHat", "funny china hat", ModuleCategory.RENDER) {
         glPushMatrix()
 
         glTranslated(pos.x, pos.y + mc.thePlayer.height - if (mc.thePlayer.isSneaking) 0.08 else 0.0, pos.z)
+        if (rainbow) glRotated(System.currentTimeMillis() / rainbowSpeed % 360.0, 0.0, 1.0, 0.0)
 
         glEnable(GL_BLEND)
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE)
         glEnable(GL_LINE_SMOOTH)
         glLineWidth(1f)
         glDisable(GL_CULL_FACE)
         glDisable(GL_TEXTURE_2D)
         glDisable(GL_DEPTH_TEST)
         glDepthMask(false)
+        glShadeModel(GL_SMOOTH)
 
         glBegin(GL_TRIANGLE_FAN)
         glVertex3d(0.0, height.toDouble(), 0.0)
         for (i in 0..360) {
-            GLUtils.glColor(red, green, blue, 130)
+            val color = if (rainbow) Color.getHSBColor(i / 360f, 1f, 1f) else Color(red, green, blue)
+
+            GLUtils.glColor(color.red, color.green, color.blue, 130)
             glVertex3d((radius * sin(i * PI / 180)), 0.0, (radius * cos(i * PI / 180)))
         }
         glEnd()
 
         glBegin(GL_LINE_LOOP)
         for (i in 0..360) {
-            GLUtils.glColor(red, green, blue)
+            val color = if (rainbow) Color.getHSBColor(i / 360f, 1f, 1f) else Color(red, green, blue)
+
+            GLUtils.glColor(color.red, color.green, color.blue)
             glVertex3d((radius * sin(i * PI / 180)), 0.0, (radius * cos(i * PI / 180)))
         }
         glEnd()
@@ -66,6 +77,7 @@ object FunnyHat : Module("FunnyHat", "funny china hat", ModuleCategory.RENDER) {
         glEnable(GL_TEXTURE_2D)
         glEnable(GL_DEPTH_TEST)
         glDepthMask(true)
+        glShadeModel(GL_FLAT)
 
         glPopMatrix()
     }
