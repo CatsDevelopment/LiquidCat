779bd6e33c35ea6823e995340947c5e20c35434e
Shurpe <81492944+14ms@users.noreply.github.com>
2022-04-17 20:19:00 +0300

    Added simple GuiMove spoof

--- src/main/kotlin/lol/liquidcat/features/module/modules/movement/GuiMove.kt
+++ src/main/kotlin/lol/liquidcat/features/module/modules/movement/GuiMove.kt
@@ -6,13 +6,18 @@
 package lol.liquidcat.features.module.modules.movement
 
 import lol.liquidcat.event.EventTarget
+import lol.liquidcat.event.PacketEvent
 import lol.liquidcat.event.UpdateEvent
 import lol.liquidcat.features.module.Module
 import lol.liquidcat.features.module.ModuleCategory
+import lol.liquidcat.value.BoolValue
 import net.minecraft.client.gui.GuiChat
 import net.minecraft.client.settings.GameSettings
+import net.minecraft.network.play.client.C16PacketClientStatus
 
 object GuiMove : Module("GuiMove", "Allows you to walk while an inventory is opened.", ModuleCategory.MOVEMENT) {
+
+    private val spoof by BoolValue("Spoof", true)
 
     private val keys = arrayOf(
         mc.gameSettings.keyBindForward,
@@ -27,6 +32,14 @@ object GuiMove : Module("GuiMove", "Allows you to walk while an inventory is ope
     fun onUpdate(event: UpdateEvent) {
         if (mc.currentScreen !is GuiChat)
             keys.forEach { key -> key.pressed = GameSettings.isKeyDown(key) }
+    }
+
+    @EventTarget
+    fun onPacket(event: PacketEvent) {
+        val packet = event.packet
+
+        if (spoof && packet is C16PacketClientStatus && packet.status == C16PacketClientStatus.EnumState.OPEN_INVENTORY_ACHIEVEMENT)
+            event.cancelEvent()
     }
 
     override fun onDisable() {
