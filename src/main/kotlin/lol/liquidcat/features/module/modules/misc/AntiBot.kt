40d6a3dfd66ae4034117146a284b0fcdbf249662
Shurpe <81492944+14ms@users.noreply.github.com>
2022-06-15 13:09:25 +0300

    Improved AntiBot

--- src/main/kotlin/lol/liquidcat/features/module/modules/misc/AntiBot.kt
+++ src/main/kotlin/lol/liquidcat/features/module/modules/misc/AntiBot.kt
@@ -10,61 +10,63 @@ import lol.liquidcat.features.module.ModuleCategory
 import lol.liquidcat.utils.entity.ping
 import lol.liquidcat.value.BoolValue
 import lol.liquidcat.value.IntValue
-import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.player.EntityPlayer
 
-//TODO Add InvalidUUID Check
+//TODO Add check for invalid UUID using Mojang profiles API
 
 object AntiBot : Module("AntiBot", "Prevents KillAura from attacking AntiCheat bots.", ModuleCategory.MISC) {
 
     private val invalidName by BoolValue("InvalidName", true)
     private val invalidPitch by BoolValue("InvalidPitch", true)
-    private val health by BoolValue("Health", true)
+    private val invalidHealth by BoolValue("InvalidHealth", true)
+
     private val livingTime by BoolValue("LivingTime", false)
     private val livingTimeTicks by IntValue("LivingTimeTicks", 40, 1..200)
+
     private val entityId by BoolValue("EntityID", true)
 
     private val ping by BoolValue("Ping", false)
+
     private val duplicateInWorld by BoolValue("DuplicateInWorld", false)
     private val duplicateInTab by BoolValue("DuplicateInTab", true)
 
-    fun isBot(entity: EntityLivingBase): Boolean {
-        if (entity !is EntityPlayer)
-            return false
+    fun isBot(player: EntityPlayer): Boolean {
+        if (!state) return false
 
-        if (!state)
-            return false
-
-        if (invalidName && (!entity.name.contains(Regex("^[a-zA-Z0-9_]+$")) || entity.name.length !in 3..16))
+        // Checks if player name contains illegal characters or the length of the name is not valid
+        if (invalidName && (!player.name.contains(Regex("^[a-zA-Z0-9_]+$")) || player.name.length !in 3..16))
             return true
 
-        if (health && (entity.health > entity.maxHealth || entity.health.isNaN()))
+        // Checks if player's health is higher than its limit
+        if (invalidHealth && (player.health > player.maxHealth || player.health.isNaN()))
             return true
 
-        if (invalidPitch && (entity.rotationPitch > 90f || entity.rotationPitch < -90f))
+        // Checks if player's head pitch is not within its limit
+        if (invalidPitch && (player.rotationPitch > 90f || player.rotationPitch < -90f))
             return true
 
-        if (livingTime && entity.ticksExisted < livingTimeTicks)
+        if (livingTime && player.ticksExisted < livingTimeTicks)
             return true
 
-        if (entityId && (entity.entityId >= 1000000000 || entity.entityId <= -1))
+        // Checks if player's entity ID is not valid
+        if (entityId && (player.entityId >= 1000000000 || player.entityId <= -1))
             return true
 
-        if (ping && entity.ping == 0)
+        // Checks if player's ping is 0
+        if (ping && player.ping == 0)
             return true
 
-        if (duplicateInWorld) {
-            if (mc.theWorld.loadedEntityList
-                    .count { ent -> ent is EntityPlayer && entity.gameProfile.name == ent.gameProfile.name } > 1
-            ) return true
-        }
+        // Checks if player name is duplicated in the world several times
+        if (duplicateInWorld)
+            if (mc.theWorld.loadedEntityList.count { it is EntityPlayer && player.name == it.gameProfile.name } > 1)
+                return true
 
-        if (duplicateInTab) {
-            if (mc.netHandler.playerInfoMap
-                    .count { plr -> entity.gameProfile.name == plr.gameProfile.name } > 1
-            ) return true
-        }
+        // Checks if player name is duplicated in tab several times
+        if (duplicateInTab)
+            if (mc.netHandler.playerInfoMap.count { player.name == it.gameProfile.name } > 1)
+                return true
 
+        // Returns false if all checks are passed
         return false
     }
 }
